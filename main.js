6df2a74837a59c8771dba35c077557bbff845aeb
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-21 15:14:20 +0900

    이미지 로딩 콘솔 로드

--- main.js
+++ main.js
@@ -1,27 +1,7 @@
-/*
-1. 이미지 동적으로 200개 생성
-2. 이미지 소스가 로딩이 될 때 에러가 발생하는 시스템 이벤트 설정
-3. 브라우저에서 마우스를 움직일 때마다 마우스 좌표값 구하기
-4. 특정 수치값을 백분율화 하는 로직 처리
-5. 이미지 소스가 모두 로딩되는 상태를 백분율로 변환
-*/
-
-/*
-img노드 생성
-src 속성 생성
-src 속성 노드에 value = img/pic0~100.jpg
-위 작업을 100번 반복을 돌리면서 append로 이미지 요소 반복 추가
-
-100분율 구하는 공식 
-현재 수치값 / 전체수치값 * 100 (백분율)
-현재 수치값 / 전체수치값 * 200 (이백분율)
-*/
-
+const num = 200;
 const section = document.querySelector('section');
 const imgs = creatImgs(section, 200);
-console.log(imgs);
 
-//인수로 갯수를 받아서 동적으로 img를 생성해주는 함수
 function creatImgs(target, num) {
 	for (let i = 0; i < num; i++) {
 		const img = document.createElement('img');
@@ -30,15 +10,35 @@ function creatImgs(target, num) {
 		img.setAttributeNode(src);
 		target.append(img);
 	}
-	return target.querySelectorAll('img');
+	const imgs = target.querySelectorAll('img');
+	let count = 0;
+	imgs.forEach((img) => {
+		//해당 돔에 수반되는 소스이미지가 로딩완료시 실행되는 이벤트
+		img.onload = () => {
+			count++;
+			console.log('현재 로딩된 소스 이미지', count);
+			if (count === num) {
+				//동적으로 만들어진 이미지 요소의 소스 이미지가 렌더링 완료된 시점
+				console.log('모든 소스 이미지 로딩 완료');
+			}
+		};
+	});
+	return imgs;
 }
 
 window.addEventListener('mousemove', (e) => {
+	const percent = getPercent(e, num);
+	activation(imgs, percent);
+});
+
+function getPercent(e) {
 	const curPos = e.pageX;
 	const wid = window.innerWidth;
-	const percent = parseInt((curPos / wid) * 200);
-	console.log(percent);
+	const percent = parseInt((curPos / wid) * num);
+	return percent;
+}
 
-	//parseInt(숫자) : 실수에서 소수점 아랫값을 버려서 변수로 반환
-	//parseFloat(숫자) : 소수점 아래까지 있는 실수 반환
-});
+function activation(arr, index) {
+	arr.forEach((el) => (el.style.display = 'none'));
+	arr[index].style.display = 'block';
+}
