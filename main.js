da74d713a8e8dd029b66e86d8a1d1bec1510547d
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-21 15:43:15 +0900

    퍼센트 로딩후 로딩페이지 삭제

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const num = 200;
 const section = document.querySelector('section');
+const aside = document.querySelector('aside');
+const lodingNum = document.querySelector('aside p span');
 const imgs = creatImgs(section, 200);
 
 function creatImgs(target, num) {
@@ -16,10 +18,13 @@ function creatImgs(target, num) {
 		//해당 돔에 수반되는 소스이미지가 로딩완료시 실행되는 이벤트
 		img.onload = () => {
 			count++;
+			const percent = parseInt((count / num) * 100);
+			lodingNum.innerText = percent;
 			console.log('현재 로딩된 소스 이미지', count);
 			if (count === num) {
 				//동적으로 만들어진 이미지 요소의 소스 이미지가 렌더링 완료된 시점
 				console.log('모든 소스 이미지 로딩 완료');
+				aside.remove();
 			}
 		};
 	});
